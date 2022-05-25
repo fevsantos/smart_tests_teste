9e47a009c9dee0aab84682e3c6403e0d112521bd
Fernanda Santos <fevsantos@gmail.com>
2022-05-25 16:58:12 -0300

    users e questions

---
+++ db/migrate/20220525195403_add_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :uid, :string
+    add_column :users, :avatar_url, :string
+    add_column :users, :provider, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_161426) do
+ActiveRecord::Schema.define(version: 2022_05_25_195403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,10 @@ ActiveRecord::Schema.define(version: 2022_05_25_161426) do
     t.boolean "teacher_role", default: false
     t.boolean "student_role", default: true
     t.string "name"
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
