f911750e2ab91fac9bf9dd51b24109694d7d9859
Fernanda Santos <fevsantos@gmail.com>
2022-05-25 10:29:55 -0300

    quiz semi pronto

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,28 @@
+class QuizzesController < ApplicationController
+
+  def index
+    @quizzes = Quiz.all
+  end
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def new
+    @quiz = Test.new
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
+      redirect_to quiz_path(@quiz)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:name)
+  end
+end
