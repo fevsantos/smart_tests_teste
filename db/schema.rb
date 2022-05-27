264ffd8db7b093977cc735c5da29e10868f2a843
Fernanda Santos <fevsantos@gmail.com>
2022-05-27 18:14:04 -0300

    ultimas alterações

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_184547) do
+ActiveRecord::Schema.define(version: 2022_05_27_131926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2022_05_26_184547) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "alternatives", force: :cascade do |t|
+    t.string "name"
+    t.boolean "correct", default: false
+    t.bigint "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_alternatives_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
